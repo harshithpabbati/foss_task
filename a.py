3018302a7279e3916e577f2877fa93b4e37fa96e
Harshithpabbati <43822585+Harshithpabbati@users.noreply.github.com>
2018-10-26 00:02:02 +0530

    gmail through cli

---
+++ a.py
@@ -0,0 +1,11 @@
+import smtplib
+content=input("enter the text to enter in mail:")
+a=input("your mail address:")
+b=input("your password:")
+c=input("recievers mail address:")
+mail=smtplib.SMTP('smtp.gmail.com',587)
+mail.ehlo()
+mail.starttls()
+mail.login(a,b)
+mail.sendmail(a,c,content)
+mail.close()
