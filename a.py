1e532dd0d6f74465614738495fb950bbeafec518
Harshithpabbati <43822585+Harshithpabbati@users.noreply.github.com>
2018-10-27 00:38:26 +0530

    gmail through cli

--- a.py
+++ a.py
@@ -1,7 +1,8 @@
 import smtplib
+import getpass
 content=input("enter the text to enter in mail:")
 a=input("your mail address:")
-b=input("your password:")
+b=getpass.getpass("your password:")
 c=input("recievers mail address:")
 mail=smtplib.SMTP('smtp.gmail.com',587)
 mail.ehlo()
